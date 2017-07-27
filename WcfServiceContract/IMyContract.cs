2fd23ffab7113fc7188eba39fe60a8fa2b22f388
刘志辉 <879651072@qq.com>
2017-07-27 17:30:14 +0800

    1.1 服务契约

---
+++ WcfServiceContract/IMyContract.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ServiceModel;
+
+namespace WcfServiceContract
+{
+    [ServiceContract]
+    internal interface IMyContract
+    {
+        [OperationContract]
+        String MyMethod(string text);
+
+        [OperationContract]
+        string MyOtherMethod(string text);
+    }   
+
+}
---
+++ WcfServiceContract/MyService.svc.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceContract
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码、svc 和配置文件中的类名“MyService”。
+    public class MyService : IMyContract, IService1
+    {
+        string IMyContract.MyMethod(string text)
+        {
+            return "Hello " + text;
+        }
+
+        public string MyOtherMethod(string text)
+        {
+            return "Can not call this method over wcf";
+        }
+
+        public string GetData(int data)
+        {
+            return data.ToString();
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
