3df4722d2fb363376ed6653155a58af57834d92d
刘志辉 <879651072@qq.com>
2017-12-15 16:08:08 +0800

    WCF服务编程(3)——以管理的方式配置终结点

---
+++ WCF服务编程(3)——以管理的方式配置终结点/服务端/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace WindowsFormsApplication6
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Uri tcpBaseAddress = new Uri(@"http://localhost:8001/");
+            ServiceHost host = new ServiceHost(typeof(Service1));
+            System.ServiceModel.Channels.Binding wsBinding = new WSHttpBinding();
+            //host.AddServiceEndpoint(typeof(IService1), wsBinding, "http://localhost:8002/");
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
