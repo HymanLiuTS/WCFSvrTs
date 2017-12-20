d59e0696edcf0d2f506f6889763a386da268178c
刘志辉 <879651072@qq.com>
2017-12-20 20:28:38 +0800

    WCF服务编程(5)——通过Configure()方法配置终结点

---
+++ WCF服务编程(5)——通过Configure()方法配置终结点/服务端/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Uri uri = new Uri("http://localhost:8001/Service1/");
+            ServiceHost host = new ServiceHost(typeof(Service1),uri);
+            //ServiceHost host1 = new ServiceHost(typeof(Service1));
+            
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
---
+++ WCF服务编程(5)——通过Configure()方法配置终结点/服务端/Service1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
+    public class Service1 :BaseService, IService1
+    {
+        public new static void Configure(ServiceConfiguration config)
+        {
+            BaseService.Configgure(config);
+            WSHttpBinding wsBinding = new WSHttpBinding();
+            //config.AddServiceEndpoint(typeof(IService1), wsBinding, "");
+            //config.Description.Behaviors.Add(new ServiceMetadataBehavior { HttpGetEnabled = true });
+            //config.EnableProtocol(new WSHttpBinding()); 
+            config.LoadFromConfiguration();
+
+        }
+        public void DoWork()
+        {
+            MessageBox.Show("我被调用了");
+        }
+    }
+}
