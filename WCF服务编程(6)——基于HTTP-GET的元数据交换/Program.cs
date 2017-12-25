7e904185c0caa92ab99e4aa220774aebb678638d
刘志辉 <879651072@qq.com>
2017-12-25 19:04:40 +0800

    WCF服务编程(6)——基于HTTP-GET的元数据交换

---
+++ WCF服务编程(6)——基于HTTP-GET的元数据交换/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication9
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Uri uri = new Uri(@"http://localhost:8001");
+            ServiceHost host = new ServiceHost(typeof(Service1), uri);
+            ServiceMetadataBehavior metadataBehavior = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior();
+                metadataBehavior.HttpGetEnabled = true;
+                host.Description.Behaviors.Add(metadataBehavior);
+            }
+            host.AddDefaultEndpoints();
+
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
