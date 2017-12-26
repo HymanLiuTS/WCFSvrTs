44ea5dca4e94eec210088296b151924d0164ea2c
刘志辉 <879651072@qq.com>
2017-12-26 20:58:46 +0800

    WCF服务编程(7)——配置元数据交换终结点

---
+++ WCF服务编程(7)——配置元数据交换终结点/以编程的方式配置元数据交换终结点/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+
+namespace WindowsFormsApplication12
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ServiceHost host = new ServiceHost(typeof(Service1));
+            ServiceMetadataBehavior metadataBehavior;
+            metadataBehavior = host.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior();
+                host.Description.Behaviors.Add(metadataBehavior);
+            }
+            //以创建ServiceMetadataEndpoint对象的方式添加元数据终结点
+            EndpointAddress address = new EndpointAddress("net.tcp://localhost:8001/service1/mex");
+            System.ServiceModel.Channels.Binding binding = MetadataExchangeBindings.CreateMexTcpBinding();
+            ServiceEndpoint endpoint = new ServiceMetadataEndpoint(binding,address);
+            host.AddServiceEndpoint(endpoint);
+            //以创建binding的形式部分元数据终结点
+            //System.ServiceModel.Channels.Binding binding = MetadataExchangeBindings.CreateMexHttpBinding();
+            //host.AddServiceEndpoint(typeof(IMetadataExchange), binding, "Mex");
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
---
+++ WCF服务编程(7)——配置元数据交换终结点/以配置文件的方式配置元数据交换终结点/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+using System.ServiceModel;
+
+namespace WindowsFormsApplication11
+{
+    static class Program
+    {
+        /// <summary>binding
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ServiceHost host = new ServiceHost(typeof(Service1));
+
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
