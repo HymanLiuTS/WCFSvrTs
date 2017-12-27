64e8753ed0241f67d92a9a57d8e3ad95a5b89092
刘志辉 <879651072@qq.com>
2017-12-27 19:54:01 +0800

    WCF服务编程(7)——配置元数据交换终结点

---
+++ WCF服务编程(7)——配置元数据交换终结点/实现ServiceHost进行元数据终结点的配置/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication14
+{
+    static class Program
+    {
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            ServiceHost<Service1> host = new ServiceHost<Service1>();
+            //host.EnableMetaDataExchange(true);
+            host.AddAllMexEndpoint();
+            host.Open();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            host.Close();
+        }
+    }
+}
---
+++ WCF服务编程(7)——配置元数据交换终结点/实现ServiceHost进行元数据终结点的配置/ServiceHost.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.Diagnostics;
+
+namespace WindowsFormsApplication14
+{
+    public class ServiceHost<T>:ServiceHost
+    {
+        public ServiceHost():base(typeof(T))
+        {
+            
+        }
+
+        public void EnableMetaDataExchange(bool enableHttpGet = true)
+        {
+            if (State == CommunicationState.Opened)
+            {
+                throw new InvalidOperationException("Host is alread opened");
+            }
+            ServiceMetadataBehavior metadataBehavior = this.Description.Behaviors.Find<ServiceMetadataBehavior>();
+            if (metadataBehavior == null)
+            {
+                metadataBehavior = new ServiceMetadataBehavior();
+                Description.Behaviors.Add(metadataBehavior);
+                if (BaseAddresses.Any(uri => uri.Scheme == "http"))
+                {
+                    metadataBehavior.HttpGetEnabled = enableHttpGet;
+                }
+            }
+            AddAllMexEndpoint();
+        }
+
+        public bool HasMexEndPoint
+        {
+            get
+            {
+                return Description.Endpoints.Any(endpoint => endpoint.Contract.ContractType == typeof(IMetadataExchange));
+            }
+        }
+
+        public void AddAllMexEndpoint()
+        {
+            Debug.Assert(HasMexEndPoint == false);
+            foreach (Uri baseAddress in BaseAddresses)
+            {
+                System.ServiceModel.Channels.Binding binding = null;
+                switch (baseAddress.Scheme)
+                {
+                    case "net.tcp":
+                        binding = MetadataExchangeBindings.CreateMexTcpBinding();
+                        break;
+                    case "net.pipe":
+                        binding = MetadataExchangeBindings.CreateMexNamedPipeBinding();
+                        break;
+                    case "http":
+                        binding = MetadataExchangeBindings.CreateMexHttpBinding();
+                        break;
+                    case "https":
+                        binding = MetadataExchangeBindings.CreateMexHttpsBinding();
+                        break;
+                }
+                if (binding != null)
+                {
+                    AddServiceEndpoint(typeof(IMetadataExchange), binding, "Mex");
+                }
+            }
+        }
+        
+    }
+}
